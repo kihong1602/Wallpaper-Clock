471389b5bc7496bf098e43916495f25924a7dded
Kihong <kks4517@naver.com>
2024-11-04 16:37:59 +0900

    Update Clock Format ver.Java

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 function updateTime() {
-	const now = new Date();
-	const hours = now.getHours();
-	const minutes = now.getMinutes();
-	const seconds = now.getSeconds();
-	const month = now.getMonth();
-	const day = now.getDate();
-	const year = now.getFullYear();
-	const period = hours >= 12 ? '"PM"' : '"AM"';
-	const hours12 = hours % 12 === 0 ? 12 : hours % 12;
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const month = now.getMonth();
+  const day = now.getDate();
+  const year = now.getFullYear();
+  const period = hours >= 12 ? '"PM"' : '"AM"';
+  const hours12 = hours % 12 === 0 ? 12 : hours % 12;
 
-	// Display the time parts in the corresponding span elements
-	document.getElementById('hour').textContent = hours12.toString().padStart(2, '0');
-	document.getElementById('minute').textContent = minutes.toString().padStart(2, '0');
-	document.getElementById('second').textContent = seconds.toString().padStart(2, '0');
-	document.getElementById('year').textContent = year.toString().padStart(2, '0');
-	document.getElementById('day').textContent = day.toString().padStart(2, '0');
-	document.getElementById('period').textContent = period;
+  // Convert month number to month name
+  const monthNames = [
+    "\"January\"", "\"February\"", "\"March\"",
+    "\"April\"", "\"May\"", "\"June\"",
+    "\"July\"", "\"August\"", "\"September\"",
+    "\"October\"", "\"November\"", "\"December\""
+  ];
 
-	// Convert month number to month name
-	const monthNames = [
-		"\"January\"", "\"February\"", "\"March\"",
-		"\"April\"", "\"May\"", "\"June\"",
-		"\"July\"", "\"August\"", "\"September\"",
-		"\"October\"", "\"November\"", "\"December\""
-	];
-	document.getElementById('month').textContent = monthNames[month];
+  // Display the time parts in the corresponding span elements
+  document.getElementById('year').textContent = year.toString().padStart(2, '0');
+  document.getElementById('month').textContent = monthNames[month];
+  document.getElementById('day').textContent = day.toString().padStart(2, '0');
+  document.getElementById('period').textContent = period;
+  document.getElementById('hour').textContent = hours12.toString().padStart(2, '0');
+  document.getElementById('minute').textContent = minutes.toString().padStart(2, '0');
+  document.getElementById('second').textContent = seconds.toString().padStart(2, '0');
 
 }
 

f4b58b8f462a566b015b81b498a4b812c04228c4
Kihong <kks4517@naver.com>
2024-11-04 20:59:58 +0900

    Add DayOfWeek

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function updateTime() {
   const seconds = now.getSeconds();
   const month = now.getMonth();
   const day = now.getDate();
+  const dayOfWeek = now.getDay();
   const year = now.getFullYear();
   const period = hours >= 12 ? '"PM"' : '"AM"';
   const hours12 = hours % 12 === 0 ? 12 : hours % 12;
@@ -17,10 +18,14 @@ function updateTime() {
     "\"October\"", "\"November\"", "\"December\""
   ];
 
+  const dayOfWeeks = ["\"Sunday\"", "\"Monday\"", "\"Tuesday\"", "\"Wednesday\"", "\"Thursday\"", "\"Friday\"",
+    "\"Saturday\""];
+
   // Display the time parts in the corresponding span elements
   document.getElementById('year').textContent = year.toString().padStart(2, '0');
   document.getElementById('month').textContent = monthNames[month];
   document.getElementById('day').textContent = day.toString().padStart(2, '0');
+  document.getElementById('dayOfWeek').textContent = dayOfWeeks[dayOfWeek];
   document.getElementById('period').textContent = period;
   document.getElementById('hour').textContent = hours12.toString().padStart(2, '0');
   document.getElementById('minute').textContent = minutes.toString().padStart(2, '0');
